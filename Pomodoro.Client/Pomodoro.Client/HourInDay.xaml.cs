1d1ab5b075d40e4bff9775c3de39d565f1ea7dfd
Kamal Kalyanji <kamal.kalyanji@tradelogicltd.com>
2018-09-12 14:58:25 +0100

    initial checkin

---
+++ Pomodoro.Client/Pomodoro.Client/HourInDay.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pomodoro.Client
+{
+    /// <summary>
+    /// Interaction logic for HourInDay.xaml
+    /// </summary>
+    public partial class HourInDay : UserControl
+    {
+        public string Hour
+        {
+            get { return lblHour.Content.ToString(); }
+            set { lblHour.Content = value; }
+        }
+
+        public Visibility ImgVisibility
+        {
+            get { return image.Visibility; }
+            set { image.Visibility = value; }
+        }
+
+
+        public HourInDay()
+        {
+            InitializeComponent();
+            image.Source = new BitmapImage(new Uri("greenTick.jpg", UriKind.Relative));
+        }
+    }
+}
---
+++ Pomodoro.Client/Pomodoro.Client/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Pomodoro.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int PomoCount { get; set; }
+
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            lblPomoCount.Content = 0;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+            // image.Source = new BitmapImage(new Uri("greenTick.jpg", UriKind.Relative));
+            //  textBox.IsEnabled = false;
+            Task.Run(() => Time());
+        }
+
+
+        private void Time()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            TimeSpan timeSpan = new TimeSpan(0, 25, 0);
+            TimeSpan countdown = timeSpan;
+            while (countdown > new TimeSpan(0, 0, 0)) 
+            {
+                countdown = timeSpan - sw.Elapsed;
+
+                this.Dispatcher.Invoke(() => {
+                    lblCountdownTimer.Content = countdown;
+                });
+            }
+            
+            this.Dispatcher.Invoke(() => {
+                lblCountdownTimer.Content = timeSpan;
+                lblPomoCount.Content = PomoCount++;
+            });
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
